0f46ae002e4a08a4ee6cc1dd198d1c1691392c2a
MichalAW <michal.wanta@gmail.com>
2019-05-16 20:38:05 +0200

    clean spaces, fix package.json

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ class App extends React.Component {
 			searchText: "",
 			users: []
 		};
-  	}
+  	};
 
 	onChangeHandle(event) {
-    	this.setState({ searchText: event.target.value });
- 	}
+    	this.setState({ searchText: event.target.value});
+ 	};
 
 	onSubmit(event) {
 		event.preventDefault();
-		const { searchText } = this.state;
+		const {searchText} = this.state;
 		const url = `https://api.github.com/search/users?q=${searchText}`;
 		fetch(url)
 		.then(response => response.json())
-		.then(responseJson => this.setState({ users: responseJson.items }));
-  	}
+		.then(responseJson => this.setState({ users: responseJson.items}));
+  	};
 
   	render() {
     	return (
@@ -32,33 +32,31 @@ class App extends React.Component {
 						value={this.state.searchText}
 					/>
 				</form>
-			<UsersList users={this.state.users} />
+				<UsersList users={this.state.users}/>
 			</div>
     	);
-  	}
-}
+  	};
+};
 
 class UsersList extends React.Component {
 	get users() {
-    	return this.props.users.map(user => <User key={user.id} user={user} />);
+    	return this.props.users.map(user => <User key={user.id} user={user}/>);
 	}
 
 	render() {
     	return <div>{this.users}</div>;
-	}
-}
+	};
+};
 
 class User extends React.Component {
 	render() {
     	return (
 			<div>
-				<img src={this.props.user.avatar_url} style={{ maxWidth: "100px" }} />
-				<a href={this.props.user.html_url} target="_blank">
-					{this.props.user.login}
-				</a>
+				<img src={this.props.user.avatar_url} style={{maxWidth: "100px"}}/>
+				<a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
 			</div>
     	);
-  	}
-}
+  	};
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
