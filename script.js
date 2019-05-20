f413f4dd9bd06fcf1fa1f0aaa5ecfc7e315300d3
MichalAW <michal.wanta@gmail.com>
2019-05-20 20:26:59 +0200

    finish

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ class App extends React.Component {
   	};
 
 	onChangeHandle(event) {
-    	this.setState({ 
+		this.setState({ 
 			searchText: event.target.value
 		});
- 	};
+	};
 
 	onSubmit(event) {
 		event.preventDefault();
@@ -22,8 +22,8 @@ class App extends React.Component {
 		.then(responseJson => this.setState({ users: responseJson.items}));
   	};
 
-  	render() {
-    	return (
+	render() {
+		return (
 			<div>
 				<form onSubmit={event => this.onSubmit(event)}>
 					<label htmlFor="searchText">Search by user name</label>
@@ -33,36 +33,32 @@ class App extends React.Component {
 						onChange={event => this.onChangeHandle(event)}
 						value={this.state.searchText}
 					/>
-				</form>
+					</form>
 				<UsersList users={this.state.users}/>
 			</div>
-    	);
-  	};
+		);
+	};
 };
 
 class UsersList extends React.Component {
 	get users() {
-		return 
-			this.props.users.map(user => <User key={user.id} user={user}/>);
-	};
+		return this.props.users.map(user => <User key={user.id} user={user}/>);
+};
 
 	render() {
-		return 
-			<div>
-				{this.users}
-			</div>;
+		return <div>{this.users}</div>;
 	};
 };
 
 class User extends React.Component {
 	render() {
-    	return (
+		return (
 			<div>
 				<img src={this.props.user.avatar_url} style={{maxWidth: "100px"}}/>
 				<a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
 			</div>
-    	);
-  	};
+		);
+	};
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
