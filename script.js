bc4f1f1ebdd934407c1241af2ec919de782b8db7
MichalAW <michal.wanta@gmail.com>
2019-05-20 20:42:25 +0200

    fix

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ class App extends React.Component {
 class UsersList extends React.Component {
 	get users() {
 		return this.props.users.map(user => <User key={user.id} user={user}/>);
-};
+	};
 
 	render() {
 		return <div>{this.users}</div>;
